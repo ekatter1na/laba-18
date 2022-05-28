c2bad6a77be502a416a395b52575a7b39cb63ebc
ekatter1na <98614159+ekatter1na@users.noreply.github.com>
2022-05-28 08:30:16 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+package com.metanit;
+
+import java.util.Scanner;
+
+class Calculate{
+    private static Calculate calculate= new Calculate();
+    private double x;
+    private double y;
+    private Calculate() {
+    }
+    public static Calculate getInstanse(){
+        return calculate;
+    }
+    public double counting() {
+        return 2 * x + 3 / y;
+    }
+    public void setChicel(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculate calcul = Calculate.getInstanse();
+        Scanner i = new Scanner(System.in);
+        System.out.println("Введите значение x");
+        double x= i.nextDouble();
+        System.out.println("Введите значение y");
+        double y = i.nextDouble();
+        calcul.setChicel(x, y);
+        System.out.println("2*x+3/y= " + calcul.counting());
+    }
+}
