87c5f1e66330c9368fabb19451e17d6e26b4abe0
ekatter1na <98614159+ekatter1na@users.noreply.github.com>
2022-05-28 10:17:07 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,37 +2,34 @@ package com.metanit;
 
 import java.util.Scanner;
 
-public class Main {
-    public static final class Calculate{
-        private static Main.Calculate calcul = null;
-        private double x;
-        private double y;
-
-        private Calculate() {
-            this.x = x;
-            this.y = y;
-        }
-
-        public synchronized Main.Calculate getInstance() {
-            if (calcul == null)
-                calcul = new Main.Calculate();
-            return calcul;
-        }
-        public void prim(double x,double y){
-            double result = (2*x+3)/y;
-            System.out.println("результат: " + result);
-        }
-
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            System.out.println("введите x:");
-            double x = in.nextDouble();
-            System.out.println("введите y:");
-            double y = in.nextDouble();
-            Calculate calk = new Calculate();
-            calk.prim(x,y);
-        }
-
+class Calculate{
+    private static Calculate calcul= new Calculate();
+    private double x;
+    private double y;
+    private Calculate() {
     }
+    public static Calculate getInstanse(){
+        return calcul;
+    }
+    public double counting() {
+        return 2 * x + 3 / y;
+    }
+    
+    public void setChicel(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
+public class Main {
+    public static void main(String[] args) {
+        Calculate calcul = Calculate.getInstanse();
+        Scanner i = new Scanner(System.in);
+        System.out.println("Введите значение x");
+        double x= i.nextDouble();
+        System.out.println("Введите значение y");
+        double y = i.nextDouble();
+        calcul.setChicel(x, y);
+        System.out.println("2*x+3/y= " + calcul.counting());
+    }
 }
